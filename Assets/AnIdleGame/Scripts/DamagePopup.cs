8133443f4be026cb425021f56820ad9f66433e0e
Sonny Michael <isonnymichael@gmail.com>
2025-03-19 13:41:52 +0700

    feat: add docblock damagepopup

--- Assets/AnIdleGame/Scripts/DamagePopup.cs
+++ Assets/AnIdleGame/Scripts/DamagePopup.cs
@@ -2,7 +2,9 @@
 using UnityEngine.UI;
 using System.Collections;
 
-//script set the damagepopup text
+/*
+* This script is for controlling a popup that appears when an enemy or player gets hit by something 
+*/
 public class DamagePopup : MonoBehaviour {
 
 	public void Show (int damage)
