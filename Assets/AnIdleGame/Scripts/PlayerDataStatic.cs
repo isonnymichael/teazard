d34eb39b6e0f4ca55a7a8da4a68d7d9a574617be
Sonny Michael <isonnymichael@gmail.com>
2025-03-19 13:51:06 +0700

    feat: add doc player data static

--- Assets/AnIdleGame/Scripts/PlayerDataStatic.cs
+++ Assets/AnIdleGame/Scripts/PlayerDataStatic.cs
@@ -1,3 +1,6 @@
+/*
+* This script is responsible for saving and loading the data of a game to an XML file using Unity's built-in XmlSerializer, which allows you serialize objects into xml files that can be read back later.  It also includes methods in this class used by other scripts such as StartAttack() & StopAttack().
+*/
 public static class PlayerDataStatic{
     public static PlayerData playerData;
 }
